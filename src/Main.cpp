38aa061b563973b1da273af389141c408b32ac1b
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2023-11-13 00:41:01 -0300

    Coletando video da web cam e exibindo-o em uma janela

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,8 +6,36 @@ using namespace cv;
 
 int main()
 {
+    // Abre a webcam
+    VideoCapture cap(0);
 
-    cout << CV_VERSION << endl;
+    if (!cap.isOpened())
+    {
+        cerr << "Erro ao abrir a webcam." << endl;
+        return -1;
+    }
+
+    // Define o tamanho do frame
+    cap.set(CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(CAP_PROP_FRAME_HEIGHT, 480);
+
+    // Loop principal
+    while (true)
+    {
+        Mat frame;
+        cap.read(frame);
+
+        // Exibe o frame
+        imshow("Webcam", frame);
+
+        // Sai do loop se a tecla 'q' for pressionada
+        if (waitKey(1) == 'q')
+            break;
+    }
+
+    // Libera os recursos
+    cap.release();
+    destroyAllWindows();
 
     return 0;
 }
