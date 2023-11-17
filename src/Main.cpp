e583da971aff13191c312fbc355efa256b4ca26e
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2023-11-17 10:37:57 -0300

    BORDAS

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,43 +1,53 @@
 #include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include "HandTrackingModule.hpp" // Assuming you have a hand tracking module header file
 
 int main() {
-    cv::VideoCapture video(0, cv::CAP_DSHOW);
+    // Abre a webcam
+    cv::VideoCapture cap(0);
 
-    video.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
-    video.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
+    // Verifica se a webcam foi aberta com sucesso
+    if (!cap.isOpened()) {
+        std::cerr << "Erro ao abrir a webcam." << std::endl;
+        return -1;
+    }
 
-    HandDetector detector(0.8, 1);
+    // Cria uma janela para exibir o vídeo
+    cv::namedWindow("Webcam", cv::WINDOW_NORMAL);
 
-    std::vector<int> distPixels = {300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57};
-    std::vector<int> distCM = {20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100};
-    cv::Mat coef;
-    cv::polyfit(distPixels, distCM, 2, coef);
-
+    
     while (true) {
-        cv::Mat img;
-        video.read(img);
+        cv::Mat frame;
+        
+        // Captura um frame da webcam
+        cap >> frame;
 
-        std::vector<Hand> hands = detector.findHands(img, false);
-
-        if (!hands.empty()) {
-            auto lmlist = hands[0].lmList;
-            auto [x, y, w, h] = hands[0].bbox;
-            auto [x1, y1, _] = lmlist[5];
-            auto [x2, y2, _] = lmlist[17];
-            auto dist = std::abs(x2 - x1);
-            auto distCMT = coef.at<double>(0) * std::pow(dist, 2) + coef.at<double>(1) * dist + coef.at<double>(2);
-
-            std::cout << dist << " " << distCMT << std::endl;
-            cv::rectangle(img, cv::Point(x, y), cv::Point(x + w, y + h), cv::Scalar(255, 0, 255), 3);
-            cv::putText(img, std::to_string(static_cast<int>(distCMT)) + " cm", cv::Point(x + 5, y - 10), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 0, 255));
+        // Verifica se o frame foi capturado com sucesso
+        if (frame.empty()) {
+            std::cerr << "Erro ao capturar o frame." << std::endl;
+            break;
         }
 
-        cv::imshow("Imagem", img);
-        cv::waitKey(1);
+        // Converte o frame para escala de cinza
+        cv::Mat gray;
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+
+        // Aplica o filtro Canny
+        cv::Mat edges;
+        cv::Canny(gray, edges, 50, 150);
+       
+        // Exibe o frame com a detecção de corpos e a segmentação de bordas na janela
+        cv::imshow("Webcam", edges);
+
+        // Aguarda 30 milissegundos. Se a tecla 'ESC' for pressionada, encerra o loop.
+        if (cv::waitKey(30) == 27) {
+            break;
+        }
     }
+
+    // Libera os recursos da webcam
+    cap.release();
+
+    // Fecha a janela
+    cv::destroyAllWindows();
 
     return 0;
 }
